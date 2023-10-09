6cca7e4c80fd4fb3f5b1850a297eae6d31ee9399
Garrett Couse <garrettc305@gmail.com>
2023-10-09 16:55:30 -0500

    update

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,15 +1,9 @@
-self.addEventListener("install", function() {
-    console.log("install");
-    });
-    self.addEventListener("activate", function() {
-    console.log("activate");
-    });
-    self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function(event) {
     if (event.request.url.includes("bootstrap.min.css")) {
     console.log("Fetch request for:", event.request.url);
     event.respondWith(
     new Response(
-    ".hotel-slogan {background: green!important;} nav {display:none}",
+    ".hotel-slogan {background: red!important;} nav {display:none}",
     { headers: { "Content-Type": "text/css" }}
     )
     );
